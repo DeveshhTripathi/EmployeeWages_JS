55acb7db1474d9fffe9e643f6a5e6290f5769118
DeveshhTripathi <deveshht591@gmail.com>
2025-03-21 00:37:48 +0530

    [Devesh Tripathi] add. Daily wages calculation

--- ProjectCode.js
+++ ProjectCode.js
@@ -4,8 +4,36 @@ let isPresent = Math.floor(Math.random()*10)%2;
 
 if(isPresent == present){
     console.log("Employee is Present");
-    return;
 }
 else{
     console.log("Employee is not Present");
+    return;
 }
+
+let workingHours = Math.floor(Math.random()*10)%3;
+
+const isPartTime = 1;
+const isFullTime = 2;
+const partTime = 4;
+const fullTime = 8;
+const wagePerHour = 20;
+
+switch(workingHours){
+
+    case isPartTime:
+        workingHours = partTime;
+        break;
+
+    case isFullTime:
+        workingHours = fullTime;
+        break;
+
+    default :
+        workingHours = 0;
+        break;
+
+}
+
+let wage = wagePerHour*workingHours;
+
+console.log("Daily wage generated for employee is ",wage);
