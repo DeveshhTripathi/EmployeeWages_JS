021ab4dc63c010767d4fae557623b2ab900f53fc
DeveshhTripathi <deveshht591@gmail.com>
2025-03-20 23:54:07 +0530

    [Devesh Tripathi] add. Employee Presence check code

---
+++ ProjectCode.js
@@ -0,0 +1,11 @@
+const present = 1;
+
+let isPresent = Math.floor(Math.random()*10)%2;
+
+if(isPresent == present){
+    console.log("Employee is Present");
+    return;
+}
+else{
+    console.log("Employee is not Present");
+}
